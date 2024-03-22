0318c2e01ff6222ca53155d6530ca8c9da6cffb5
Irhankhan <163430569+Irhankhan@users.noreply.github.com>
2024-03-22 15:19:35 +0500

    Add files via upload

---
+++ Govner House/app.js
@@ -0,0 +1,36 @@
+var email = "irhan@gamil.com";
+var pass = "12345";
+if (email == "irhan@gmail.com" && pass == "12345") {
+    console.log("successfully logged ");
+}
+else {
+    console.log("invalid creds");
+}
+var percentage = 76;
+if (percentage >= 90) {
+    console.log("A+ grade ");
+}
+else if (percentage >= 80 &&
+    percentage <= 89.99) {
+    console.log("A Grade");
+}
+else if (percentage >= 70 && percentage <= 79.99) {
+    console.log("B Grade");
+}
+else if (percentage >= 60 && percentage <= 69.99) {
+    console.log("F Grade ");
+}
+var percentage3 = 95;
+if (percentage >= 90 && percentage < 100) {
+    console.log("A+ grade ");
+}
+else if (percentage >= 80 &&
+    percentage <= 89.99) {
+    console.log("A Grade");
+}
+else if (percentage >= 70 && percentage <= 79.99) {
+    console.log("B Grade");
+}
+else if (percentage >= 60 && percentage <= 69.99) {
+    console.log("F Grade ");
+}
