12d78005e70e1577376bc1f053ef6687a0119861
Irhankhan <163430569+Irhankhan@users.noreply.github.com>
2024-03-22 15:35:52 +0500

    Add files via upload

---
+++ step00/app.ts
@@ -0,0 +1,48 @@
+ let num1: number=10;
+ let num2 : number=33;
+ let result = num1 + num2
+ console.log(result)
+
+ let num3: number=12
+ let num4: number=13 
+ let num5: number=14
+  console.log(num3* num4/ num5)
+ let firsttName = "irhan"
+ console.log(firsttName)
+
+
+ let apple=5
+ let bags=3 
+ let reminder = apple + bags
+ console.log(reminder)
+
+
+
+let num7=0
+console.log(num7++)
+console.log(num7)
+console.log(num7)
+console.log(num7)
+console.log(++ num7)
+
+ let a =0
+ let b=5 
+ let c
+ c = a++ + a++ + ++b + a++ + a-- + ++b
+ //0 + 1 + 6 + 2 + 3 + 7 
+
+ console.log(c)
+
+ let num9 =5  
+ let num0 =5
+ console.log(num9==num0 )
+
+ let num10 =3
+ let num11 =2
+console.log(num10==num11)
+
+
+
+
+
+
